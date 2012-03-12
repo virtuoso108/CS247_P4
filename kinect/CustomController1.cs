8d8475db6150df7eb5798ec46c852052e3211797
kyunghee <kakooyang@gmail.com>
2012-03-12 03:20:35 -0700

    new gesture

--- kinect/CustomController1.cs
+++ kinect/CustomController1.cs
@@ -21,34 +21,39 @@ namespace SkeletalTracking
             window.webBrowser1.Navigate("http://www.cmslewis.com/cs247/final_project/orig_ui/index.html");
         }
 
+        private ulong count = 0;
+
         public override void processSkeletonFrame(SkeletonData skeleton, Dictionary<int, Target> targets)
         {
-            foreach (var target in targets)
+            if (count % 25 == 0) //process 1 frame every 0.5 secs (30f/s)
             {
-                Target cur = target.Value;
-                int targetID = cur.id; //ID in range [1..5]
-
                 Joint leftHand = skeleton.Joints[JointID.HandLeft].ScaleTo(640, 480, window.k_xMaxJointScale, window.k_yMaxJointScale);
                 Joint rightHand = skeleton.Joints[JointID.HandRight].ScaleTo(640, 480, window.k_xMaxJointScale, window.k_yMaxJointScale);
                 Joint hip = skeleton.Joints[JointID.HipCenter].ScaleTo(640, 480, window.k_xMaxJointScale, window.k_yMaxJointScale);
                 Joint ShoulderRight = skeleton.Joints[JointID.ShoulderRight].ScaleTo(640, 480, window.k_xMaxJointScale, window.k_yMaxJointScale);
                 Joint ShoulderLeft = skeleton.Joints[JointID.ShoulderLeft].ScaleTo(640, 480, window.k_xMaxJointScale, window.k_yMaxJointScale);
+                Joint Head = skeleton.Joints[JointID.Head].ScaleTo(640, 480, window.k_xMaxJointScale, window.k_yMaxJointScale);
 
-                //Calculate how far our left hand is from the target in x and y directions
-                double deltaX_left = Math.Abs(leftHand.Position.X - cur.getXPosition());
-                double deltaY_left = Math.Abs(leftHand.Position.Y - cur.getYPosition());
+                //Calculate how far our left hand is from our left shoulder in the x and y directions.
+                double deltaX_left = Math.Abs(leftHand.Position.X - ShoulderLeft.Position.X);
+                double deltaY_left = Math.Abs(leftHand.Position.Y - ShoulderLeft.Position.Y);
 
-                //Calculate how far our right hand is from the target in both x and y directions
-                double deltaX_right = Math.Abs(rightHand.Position.X - cur.getXPosition());
-                double deltaY_right = Math.Abs(rightHand.Position.Y - cur.getYPosition());
+                //Calculate how far our rigght hand is from our right shoulder in the x and y directions.
+                double deltaX_right = Math.Abs(rightHand.Position.X - ShoulderRight.Position.X);
+                double deltaY_right = Math.Abs(rightHand.Position.Y - ShoulderRight.Position.Y);
+                double deltaZ_right = Math.Abs(rightHand.Position.Z - ShoulderRight.Position.Z);
 
-                //Calculate how far our right hand is from the target in both x and y directions
-                double deltaX_hip = Math.Abs(hip.Position.X - cur.getXPosition());
-                double deltaY_hip = Math.Abs(hip.Position.Y - cur.getYPosition());
+                //Calculate how far either hand is either above or below the head. (y direction)
+                double deltaY_right_HeadDIS = rightHand.Position.Y - Head.Position.Y;
+                double deltaY_left_HeadDIS = leftHand.Position.Y - Head.Position.Y;
+
+                //Calculate the how far either hand is from our head in the x direction.
+                double deltaX_right_Head = Math.Abs(rightHand.Position.X - Head.Position.X);
+                double deltaX_left_Head = Math.Abs(leftHand.Position.X - Head.Position.X);
 
                 //Calculate z distance between hand and hip(our measure of body position)
                 double right_stretch = Math.Abs(rightHand.Position.Z - ShoulderRight.Position.Z);
-                double right_pan = rightHand.Position.X - ShoulderRight.Position.X;
+                double left_pan = leftHand.Position.X - ShoulderLeft.Position.X;
                 double right_displacement = rightHand.Position.Y - ShoulderRight.Position.Y;
                 double left_stretch = Math.Abs(leftHand.Position.Z - hip.Position.Z);
 
@@ -64,52 +69,51 @@ namespace SkeletalTracking
                     }
                 }*/
                 //System.Console.WriteLine("Stretch: " + right_stretch);
-                System.Console.WriteLine("Pan: " + right_displacement);
-                if (right_stretch >= 0.5)
+                // System.Console.WriteLine("Pan: " + right_displacement);
+
+                System.Console.WriteLine("test test test test line ");
+                //System.Console.WriteLine("test test test test line " + deltaX_right);
+                //System.Console.WriteLine("deltaYR: " + deltaY_right);
+
+                //System.Console.WriteLine("deltaXL: " + deltaX_left);
+                //System.Console.WriteLine("deltaYL: " + deltaY_left);
+                if (deltaX_right <= 30 && deltaX_right >= 0 && deltaY_right >= 0 && deltaY_right <= 30) //Checks if right hand is in correct position before attempting to see if zoom in works, correct position is right hand in line with right shoulder.
                 {
-                    //trigger move forward
-                    //cur.setTargetSelected();
-                    window.webBrowser1.InvokeScript("DS_zoomIn");
-                    System.Console.WriteLine("Zoom in gesture recognized.");
+                    System.Console.WriteLine("Your right hand is in line with your right shoulder.");
+
+                    //System.Console.WriteLine("deltaZR: " + deltaZ_right);
+                    if (deltaZ_right >= 0.35)
+                    {
+                        //trigger zoom into map
+                        window.webBrowser1.InvokeScript("DS_zoomIn");
+                        System.Console.WriteLine("Zoom in gesture recognized.");
+                    }
+                    else if (deltaZ_right <= 0.25)
+                    {
+                        //trigger zoom out of map
+                        window.webBrowser1.InvokeScript("DS_zoomOut");
+                        System.Console.WriteLine("Zoom out gesture recongnized.");
+                    }
                 }
-                else if (right_stretch <= 0.2)
+                else if (left_pan >= 35 && deltaY_left >= 0 && deltaY_left <= 30)
                 {
-                    //trigger move backward
-                    //cur.setTargetSelected();
-                }
-                else if (right_pan >= 25.0)
-                {
-                    //trigger pan right
-                    cur.setTargetSelected();
                     //trigger pan right
                     window.webBrowser1.InvokeScript("DS_nextStep");
                     System.Console.WriteLine("Swipe right Gesture recognized.");
-
                 }
-                else if (right_pan <= -25.0)
+                else if (left_pan <= -35 && deltaY_left >= 0 && deltaY_left <= 30)
                 {
-                    //trigger pan left
-                    //cur.setTargetSelected();
                     //trigger pan left
                     window.webBrowser1.InvokeScript("DS_previousStep");
                     System.Console.WriteLine("Swipe Left Gesture Recognized.");
-
                 }
-                else if (right_displacement <= -50)
+                else if ((deltaX_left_Head <= 10 && deltaY_left_HeadDIS < 0) || (deltaX_right_Head <= 10 && deltaY_right_HeadDIS < 0))
                 {
-                    //trigger pan up
-                    //cur.setTargetSelected();
-                }
-                else if (right_displacement >= 50)
-                {
-                    //trigger pan down
-                    //cur.setTargetSelected();
-                }
-                else
-                {
-                    //cur.setTargetUnselected();
+                    //Return to current location gesture
+                    System.Console.WriteLine("Return to Current Location Gesture Recognized.");
                 }
             }
+            count++;
         }
 
         public override void controllerActivated(Dictionary<int, Target> targets)
@@ -120,8 +124,7 @@ namespace SkeletalTracking
             {
                 Target cur = target.Value;
                 cur.setTargetPosition(initX + xDelta, 75);
-                //xDelta += 150;
-                xDelta += 80;
+                xDelta += 150;
             }
         }
     }
